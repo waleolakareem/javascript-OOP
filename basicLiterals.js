0c5707a8224424b705f22bcea3cf38615d769466
Oladejo <wale@senyours.io>
2019-04-15 12:49:25 -0700

    Creat An object literally

--- basicLiterals.js
+++ basicLiterals.js
@@ -1,3 +1,17 @@
-$(document).ready(function(){
-  console.log("what");
-});
+// $(document).ready(function(){
+//   console.log("what");
+// });
+
+
+//Object Literal
+const book1 = {
+  title:'Book One',
+  author: 'John Doe',
+  year: '2013',
+  getSummary: function(){
+    return this.title + " was written by " + this.author + " in 2013";
+  }
+}
+
+console.log(book1.getSummary());
+
